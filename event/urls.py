ab56af0c99d1b26bb93fd38bd0a3c74b3c721c70
Liam G <liamg10@yahoo.co.uk>
2024-03-06 02:24:12 +0000

    configure templates folder, load static files, build skeleton base.html, configure favicon, add Bootstrap

--- event/urls.py
+++ event/urls.py
@@ -4,7 +4,9 @@
 
 from django.urls import path
 from . import views
+from .views import display_base
 
 urlpatterns = [
-    path('', views.EventList, name='home'),
+   # path('', views.EventList, name='home'),
+    path('base/', display_base, name='display_base'),
 ]
