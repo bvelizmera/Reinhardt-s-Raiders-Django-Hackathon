d0dcd59def0198b1abb1298dd11507c890a6610a
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-06 10:03:51 +0000

    Create Student and Event models.

--- event/models.py
+++ event/models.py
@@ -1,13 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Student(models.Model):
+    """
+    Stores a single student related to :models.auth.user
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100, unique=True)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    bio = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    # photo = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.username
+
+@receiver(post_save, sender=User)
+def create_student(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(user=instance, username=user.username)
+
+# @receiver(post_save, sender=User)
+# def save_student(sender, instance, **kwargs):
+#     instance.profile.save()
+
+
 class Event(models.Model):
     """
     Stores a single event related to :model:Student
     """
     name = models.CharField(max_length=200, unique=True)
     creator = models.ForeignKey(Student, 
-        on_delete=CASCADE, related_name="event_created")
+        on_delete=models.CASCADE, related_name="event_created")
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=200)
@@ -15,7 +46,8 @@ class Event(models.Model):
     max_capacity = models.IntegerField()
     interest = models.CharField(max_length=200)
     attending = models.ForeignKey(Student, 
-        on_delete=CASCADE, related_name="attending")
+        on_delete=models.CASCADE, related_name="attending")
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} on {self.date} at {self.location} | created by {self.creator}"
