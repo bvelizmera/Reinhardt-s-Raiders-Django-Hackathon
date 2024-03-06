6fe3a7f1af4763893c557f1e4b579f1ba813d8e4
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-06 17:31:33 +0000

    Add CR functionality for students with event model.

--- event/views.py
+++ event/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponse
 from django.views import generic
 from .models import Student, Event
+from .forms import EventForm
 
 # Create your views here.
 def EventList(request):
@@ -21,12 +22,26 @@ def show_user_events(request):
     """
     Display all events created by the logged-in user
     """
-    queryset = Event.objects.all()
-    event = get_object_or_404(queryset, creator=request.user)
+    queryset = Event.objects.all().filter(creator=request.user)
+    print(queryset)
+    # Handle form data
+    if request.method == "POST":
+        event_form = EventForm(data=request.POST)
+        if event_form.is_valid():
+            event = event_form.save(commit=False)
+            event.creator = request.user
+            event.save()
+            # messages.add_message(
+            #     request, messages.SUCCESS,
+            #     'Event submitted and awaiting approval'
+            # )
+    #Initialise empty form
+    event_form = EventForm()
 
     return render(request, 
         'event/user_events.html', 
-        {"event": event,
+        {"events_queryset": queryset,
+         "event_form": event_form,
         }
     )
 
