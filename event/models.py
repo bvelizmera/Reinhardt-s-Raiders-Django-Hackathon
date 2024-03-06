572c339153f9b7e06d030953aeac476d31a0d0f7
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-06 09:22:18 +0000

    Add Event model

--- event/models.py
+++ event/models.py
@@ -5,4 +5,17 @@ class Event(models.Model):
     """
     Stores a single event related to :model:Student
     """
-    
+    name = models.CharField(max_length=200, unique=True)
+    creator = models.ForeignKey(Student, 
+        on_delete=CASCADE, related_name="event_created")
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    course = models.CharField(max_length=200)
+    max_capacity = models.IntegerField()
+    interest = models.CharField(max_length=200)
+    attending = models.ForeignKey(Student, 
+        on_delete=CASCADE, related_name="attending")
+
+    def __str__(self):
+        return f"{self.name} on {self.date} at {self.location} | created by {self.creator}"
